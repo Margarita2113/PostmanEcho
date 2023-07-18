abcf9c9adbf63542b82649f6322c58d5ec30d02b
Oskina Margarita <rita_2113@mail.ru>
2023-07-19 00:28:38 +0300

    successful test,reformat code

--- src/test/java/org/example/postmanEcho/PostmanEchoTests.java
+++ src/test/java/org/example/postmanEcho/PostmanEchoTests.java
@@ -1,15 +1,17 @@
 package org.example.postmanEcho;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+
 public class PostmanEchoTests {
     @Test
     void shouldReturnSendData() {
 
         given()
                 .baseUri("https://postman-echo.com")
-                .body("21.11.1900")
+                .body("21.11.1992")
                 .when()
                 .post("/post")
                 .then().log().all()
